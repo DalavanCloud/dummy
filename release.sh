1891eceb93e6b70c3848c7664b1b86ae007769cb
Sasha Koss <koss@nocorp.me>
2017-05-23 14:04:19 +0300

    Well

--- release.sh
+++ release.sh
@@ -21,5 +21,5 @@ git config --global user.email "koss+jake-the-bot@nocorp.me"
 git config --global user.name "Jake the Bot"
 git add package.json
 git commit --message "Prepare release"
-git remote add origin-pages "https://jake-the-bot:$GITHUB_TOKEN@github.com/date-fns/dummy.git" #> /dev/null 2>&1`
+git remote add origin-pages "https://$GITHUB_TOKEN:x-oauth-basic@github.com/date-fns/dummy.git" #> /dev/null 2>&1`
 git push --set-upstream origin master
